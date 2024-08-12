06b7aa605ad46ab123073f5b1a7276e8fa50c9d9
Yullit <yuliok03@gmail.com>
2024-08-12 12:07:51 +0300

    make build

---
+++ index.fd5fa20d.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",()=>{"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});
+//# sourceMappingURL=index.fd5fa20d.js.map
